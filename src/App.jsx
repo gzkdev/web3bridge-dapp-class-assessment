d4b92cddc6551e19bf7beca0e98aec2dd67b9066
Ezihe Godswill <ezihegodswill01@gmail.com>
2025-01-28 20:37:00 +0100

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,102 @@
 import { useState } from "react";
-import greeterABI from "./abi/greeter.json";
+import abi from "./abi/assessment.json";
+import * as ethers from "ethers";
+import { useEffect } from "react";
 
-const ASSESSMENT_CONTRACT = "0x524538eaf41e8a92a444d14e11de64a08131505a";
+const contractAddress = "0x524538eaf41e8a92a444d14e11de64a08131505a";
 
 function App() {
-  const [userInput, setUserInput] = useState("");
-  const [retrievedMessage, setRetrievedMessage] = useState("");
+  const [view, setView] = useState("deposit"); // deposit | withdraw
+  const [amount, setAmount] = useState(0);
+
+  const requestAccounts = async () => {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  };
+
+  const deposit = async () => {
+    if (typeof window.ethereum == "undefined") return;
+
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const tx = await contract.deposit(amount);
+      const receipt = await tx.wait();
+      console.log("successful tx", receipt);
+    } catch (error) {
+      console.log("fail  transaction", error);
+    }
+  };
+
+  const withdraw = async () => {
+    if (typeof window.ethereum == "undefined") return;
+
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const tx = await contract.withdraw(amount);
+      const receipt = await tx.wait();
+      console.log("successful tx", receipt);
+    } catch (error) {
+      console.log("fail  transaction", error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (view === "deposit") {
+        return deposit();
+      } else if (view === "withdraw") {
+        return withdraw();
+      } else {
+        throw new Error("Action can either be withdraw or deposit");
+      }
+    } catch (e) {
+      return e;
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (typeof window.ethereum !== "undefined") {
+        await requestAccounts();
+      }
+    })();
+  }, []);
 
   return (
-    <div className="flex flex-col gap-4">
-      <input type="text" placeholder="Set your message" />
-      <button>Set Message</button>
-      <button>Get Message</button>
-      <p>Retrieved Message: </p>
+    <div className="min-h-screen w-full flex items-center justify-center border">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 items-center"
+      >
+        {/* Tabs */}
+        <div className="flex items-center p-0.5 w-[240px] outline [&>button]:grow [&>button]:px-6 [&>button]:py-2 rounded divide-x">
+          <button onClick={() => setView("deposit")}>Deposit</button>
+          <button onClick={() => setView("withdraw")}>Withdraw</button>
+        </div>
+
+        {/* Input */}
+        <input
+          className="outline h-10 rounded w-[240px] px-3"
+          type="number"
+          name="amount"
+          id="amount"
+          inputMode="decimal"
+          value={amount <= 0 ? "" : amount}
+          onChange={(e) => setAmount(Number(e.target.value))}
+        />
+
+        {/* Submit */}
+        <button
+          className="capitalize bg-blue-500 py-2 px-6 rounded outline-none border-none text-white font-semibold"
+          type="submit"
+        >
+          {view}
+        </button>
+      </form>
     </div>
   );
 }
